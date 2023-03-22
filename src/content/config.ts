841c67a7dfbcc74b42a5eace31edf248eb4a29b0
Hongbusi <coderhbs@gmail.com>
2023-03-22 18:20:43 +0800

    chore: update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({
+const posts = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -14,8 +14,7 @@ const blog = defineCollection({
       .string()
       .optional()
       .transform(str => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional(),
   }),
 })
 
-export const collections = { blog }
+export const collections = { posts }
